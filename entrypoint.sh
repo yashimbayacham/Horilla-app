030e9689e658e41dd54dfbad6219848769b0ab40
Yashim Bayacham <yashimbayacham@proton.me>
2026-05-01 18:54:42 +0100

    fix: ensure migrations run on deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Starting app..."
-
 echo "Running migrations..."
-python3 manage.py migrate || exit 1
+python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python3 manage.py collectstatic --noinput || exit 1
+python manage.py collectstatic --noinput
 
-echo "Starting Gunicorn..."
-gunicorn --bind 0.0.0.0:8000 horilla.wsgi:application
+echo "Starting server..."
+gunicorn horilla.wsgi:application --bind 0.0.0.0:$PORT
