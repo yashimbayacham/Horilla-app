7d4bddefed412673d0118ca74c77e1042cf8ca6d
Yashim Bayacham <yashimbayacham@proton.me>
2026-05-01 23:21:01 +0100

    fix: proper migrations tracking

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Applying migrations..."
-python manage.py migrate --noinput
+python manage.py migrate --noinput || exit 1
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput || exit 1
 
 echo "Starting server..."
 gunicorn horilla.wsgi:application --bind 0.0.0.0:$PORT
