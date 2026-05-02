8f11111db40be82d789ec74a25f8a600fb8680ad
Yashim Bayacham <yashimbayacham@proton.me>
2026-05-02 01:48:07 +0100

    final: stable entrypoint with collectstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+set -o errexit  # stop on errors
+
 echo "Applying migrations..."
-python manage.py migrate --noinput || exit 1
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 echo "Starting server on port $PORT..."
-
 exec gunicorn horilla.wsgi:application --bind 0.0.0.0:$PORT
