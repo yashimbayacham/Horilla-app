903aea32e521c2fc8b1be46381d389f39ad934b9
Yashim Bayacham <yashimbayacham@proton.me>
2026-05-02 21:57:28 +0100

    fix: remove superuser creation (causing timeout)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,6 @@ set -o errexit
 echo "Applying migrations..."
 python manage.py migrate --noinput
 
-echo "Creating superuser if not exists..."
-python manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-import os
-
-User = get_user_model()
-
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-if username and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-EOF
-
 echo "Collecting static files..."
 python manage.py collectstatic --noinput || true
 
