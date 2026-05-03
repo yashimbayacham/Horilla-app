642de49fede4f4f9422ffa9e0097dde04cb7bdc7
Yashim Bayacham <yashimbayacham@proton.me>
2026-05-03 17:13:54 +0100

    add safe superuser creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,15 @@ set -o errexit
 echo "Applying migrations..."
 python manage.py migrate --noinput
 
+echo "Creating superuser..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username="yashimbayacham").exists():
+    User.objects.create_superuser("yashimbayacham", "yashimbayacham@proton.me", "40RTY5EVEn%")
+EOF
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput || true
 
